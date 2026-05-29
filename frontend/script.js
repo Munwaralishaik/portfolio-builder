bb3ab14ac5df7655f168b86ff272c7dc6a3e5b17
Shaik Munwar Ali <munwarroshan786@gmail.com>
2026-05-29 17:28:53 +0530

    Protect builder page

--- frontend/script.js
+++ frontend/script.js
@@ -649,3 +649,11 @@ if (loginBtn) {
 
   });
 }
+if (window.location.pathname.includes("builder.html")) {
+  const token = localStorage.getItem("token");
+
+  if (!token) {
+    alert("Please login first");
+    window.location.href = "login.html";
+  }
+}
